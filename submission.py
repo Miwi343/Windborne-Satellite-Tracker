0df7f0011ae01585739ceb69958533fe8634e89c
Mihir Kulshreshtha <mk2664@cornell.edu>
2025-11-12 18:34:26 -0500

    Modified front end and finished submitting.

--- submission.py
+++ submission.py
@@ -7,7 +7,7 @@ data = {
     "email": "mk2664@cornell.edu",
     "role": "Junior Web Developer",
     "notes": "Relationships and community mean everything to me, and I make sure to show up as a positive, present contributor in every group I'm a part of, whether that's been on basketball, dance, or software development teams.",
-    "submission_url": "A publicly accessible URL pointing to your coding challenge -- see below",
+    "submission_url": "https://windborne-satellite-tracker.onrender.com/",
     "portfolio_url": "https://github.com/EmergenceAI/embodied-drone-agents",
     "resume_url": "https://drive.google.com/file/d/1b5D3AuRW1F7_nae6YI--eCmyo2mll1cr/view?usp=sharing",
   }
